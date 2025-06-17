e61e199a11c2abf51abac29045f9fc15f278bab2
Francesco Zanini <zaninifrancesco03@gmail.com>
2025-06-17 09:23:31 +0200

    initial commit

---
+++ src/main/java/it/uniroma3/siw_events/SiwEventsApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw_events/SiwEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
